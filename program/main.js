8e491e5b48acbe7c69dd07b8de706d4f8e3dccde
liping <liping@acoinfo.com>
2021-06-01 21:03:34 +0800

    update templates

--- program/main.js
+++ program/main.js
@@ -2,18 +2,13 @@ const Web = require('webapp');
 const bodyParser = require('middleware').bodyParser;
 const iosched = require('iosched');
 
-/* Whether the app was awakened by a shared message */
-if (ARGUMENT != undefined) {
-	console.log('Awakened by share message:', ARGUMENT);
-}
-
 // Create app.
 const app = Web.createApp();
 
 app.use(bodyParser.json());
 app.use(Web.static('./public', { index: ['index.html', 'index.htm'] }));
 
-// Register rest api here
+// Register rest api here.
 app.use('/demo', require('./routers'));
 
 // Start app.
